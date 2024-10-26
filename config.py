aa23af04bb38ee8e7ba2140fd233da045bcf0e08
˹sᴛʀᴀɴɢᴇʀ-ꭙᴅ⁹⁹˼  🇮🇳 <147219905+itzshukla@users.noreply.github.com>
2024-10-27 05:02:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 API_ID = os.getenv("API_ID", "").strip()
 API_HASH = os.getenv("API_HASH", "").strip()
-BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7293869890:AAFVop74BV2FY11djRiCAeOj32qB9ol4OWM").strip()
 MONGO_URI = os.getenv("MONGO_URI", "").strip()
 AUTH_CHANNEL = os.getenv("AUTH_CHANNEL", "").strip()
 HEROKU_API_KEY = os.getenv("HEROKU_API_KEY", "").strip()
